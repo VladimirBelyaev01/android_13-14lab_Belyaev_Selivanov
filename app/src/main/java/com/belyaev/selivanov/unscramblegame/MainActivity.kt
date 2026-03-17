e9d5d6867dc49def2057a47b16bb85d557b1cb3f
s0m1k111 <spavel1207@gmail.com>
2026-03-17 13:34:24 +0300

    Step 5: Connect ViewModel to UI

--- app/src/main/java/com/belyaev/selivanov/unscramblegame/MainActivity.kt
+++ app/src/main/java/com/belyaev/selivanov/unscramblegame/MainActivity.kt
@@ -7,11 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.LineHeightStyle
 import com.belyaev.selivanov.unscramblegame.ui.theme.UnscrambleGameTheme
+import com.belyaev.selivanov.unscramblegame.ui_model.GameScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +18,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UnscrambleGameTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    GameScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnscrambleGameTheme {
-        Greeting("Android")
     }
 }
